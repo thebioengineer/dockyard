e31a2e8a98d2b82346ef0a0080497b8cab803c56
Ellis Hughes <ehhughes@scharp.org>
2019-10-17 14:29:45 -0700

    correct accidental splice, and move check for installed docker to within specific method.

--- R/install_docker.R
+++ R/install_docker.R
@@ -8,12 +8,8 @@
 #'
 #' }
 install_docker<-function(force=FALSE){
-  if(check_docker() & force == FALSE){
-    ("`docker` is already installed.")
-  }else{
     os <- get_os()
     UseMethod("install_docker",os)
-  }
 }
 
 get_os<-function(){
@@ -32,30 +28,45 @@ get_os<-function(){
   )
 }
 
-install_docker.windows <- function(user=TRUE){
-  installer <- normalizePath(
-    file.path(tempdir(),"docker_installer.exe"),
-    wins
-    lash = "/",
-    mustWork = FALSE)
+install_docker.windows <- function(force=FALSE){
+  if(check_docker() & force == FALSE){
+    message("`docker` is already installed.\n Use `dockyard::install_docker(force=TRUE)` to force a new installation.")
+    # fun message
+    if(runif(1)>.8){
+      print_docker_logo()
+    }
+  }else{
+    installer <- normalizePath(
+      file.path(tempdir(),"docker_installer.exe"),
+      winslash = "/",
+      mustWork = FALSE)
 
-  message("Downloading Docker.\nThis may take a few minutes...\n\n")
-  download.file("https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe",
-                installer,
-                mode="wb")
-  message("\nInstalling Docker. Follow the installers instructions.\n")
-  system(paste("start", installer))
+    message("Downloading Docker.\nThis may take a few minutes...\n\n")
+    download.file("https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe",
+                  installer,
+                  mode="wb")
+    message("\nRunning Docker Installer. Follow the installers instructions.\n")
+    system2("start", installer)
+  }
   invisible()
 }
 
 install_docker.mac <- function(){
-  # installer_dmg <- normalizePath(tempfile("docker_installer",fileext=".dmg"),
-  #                              winslash = "/",
-  #                              mustWork = FALSE)
+  # if(check_docker() & force == FALSE){
+  #   message("`docker` is already installed.\n Use `dockyard::install_docker(force=TRUE)` to force a new installation")
+  # }else{
+  # installer <- normalizePath(
+  #   file.path(tempdir(),"docker_installer.dmg"),
+  #   winslash = "/",
+  #   mustWork = FALSE)
+  #
   # message("Downloading Docker.\nThis may take a few minutes...")
   # download_result <- download.file("https://download.docker.com/mac/stable/Docker.dmg",
-  #                                  installer_dmg)
+  #                                  installer)
+  # message("\nInstalling Docker. Follow the installers instructions.\n")
   # system()
+  # }
+  # invisible()
 }
 
 install_docker.unix <- function(){
@@ -64,4 +75,26 @@ install_docker.unix <- function(){
   #                              winslash = "/",
   #                              mustWork = FALSE)
   # download_result <- download.file("https:://download.docker.com/win/stable/Docker for Windows Installer.exe",installer_exe)
+}
+
+
+print_docker_logo<-function(){
+  docker_logo_ascii <-c(
+  '',
+  '                    ##        .',
+  '              ## ## ##       ==',
+  '           ## ## ## ##      ===',
+  '       /""""""""""""""""\\___/ ===',
+  '  ~~~ {~~ ~~~~ ~~~ ~~~~ ~~ ~ /  ===- ~~~',
+  '       \\______ o          __/',
+  '        \\    \\        __/',
+  '         \\____\\______/',
+  '',
+  '         |          |',
+  '      __ |  __   __ | _  __   _',
+  '     /  \\| /  \\ /   |/  / _\\ |',
+  '     \\__/| \\__/ \\__ |\\_ \\__  |',
+  '')
+
+  message(paste(docker_logo_ascii,collapse = "\n"))
 }
