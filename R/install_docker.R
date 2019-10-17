46ba73b6cf16eb95a2023e6c7e6db75dec98b31d
Ellis Hughes <ehhughes@scharp.org>
2019-10-17 13:34:45 -0700

    add ability to install docker for windows

--- R/install_docker.R
+++ R/install_docker.R
@@ -1,17 +1,19 @@
 #' @export
 #' @title Install Docker
 #' @description provide the utility to install docker locally
+#' @param force force install of latest docker version
 #' @example
 #' \donotrun{
 #' dockyard::install_docker()
 #'
 #' }
-install_docker<-function(user=TRUE){
-  if(!check_docker()){
-    stop("docker is already installed.")
+install_docker<-function(force=FALSE){
+  if(check_docker() & force == FALSE){
+    ("`docker` is already installed.")
+  }else{
+    os <- get_os()
+    UseMethod("install_docker",os)
   }
-  os <- get_os()
-  UseMethod("install_docker",os)
 }
 
 get_os<-function(){
@@ -31,27 +33,29 @@ get_os<-function(){
 }
 
 install_docker.windows <- function(user=TRUE){
-
   installer <- normalizePath(
-    file.path(tempdir(),"docker_installer.zip"),
-    winslash = "/",
+    file.path(tempdir(),"docker_installer.exe"),
+    wins
+    lash = "/",
     mustWork = FALSE)
 
-  # https://docs.docker.com/install/windows/docker-ee/#use-a-script-to-install-docker-ee
-  # https://github.com/docker/docker-ce/releases/tag/v19.03.3
-  message("Downloading Docker")
-  download.file("https://download.docker.com/win/static/stable/x86_64/docker-17.09.0-ce.zip",
-                installer)
-  unzip(installer,exdir = )
+  message("Downloading Docker.\nThis may take a few minutes...\n\n")
+  download.file("https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe",
+                installer,
+                mode="wb")
+  message("\nInstalling Docker. Follow the installers instructions.\n")
+  system(paste("start", installer))
+  invisible()
 }
 
 install_docker.mac <- function(){
-  installer_dmg <- normalizePath(tempfile("docker_installer",fileext=".dmg"),
-                               winslash = "/",
-                               mustWork = FALSE)
-  message("Downloading Docker.\nThis may take a few minutes. After downloading, the installer will open. Fo")
-  download_result <- download.file("https://download.docker.com/mac/stable/Docker.dmg",installer_dmg)
-  system()
+  # installer_dmg <- normalizePath(tempfile("docker_installer",fileext=".dmg"),
+  #                              winslash = "/",
+  #                              mustWork = FALSE)
+  # message("Downloading Docker.\nThis may take a few minutes...")
+  # download_result <- download.file("https://download.docker.com/mac/stable/Docker.dmg",
+  #                                  installer_dmg)
+  # system()
 }
 
 install_docker.unix <- function(){
