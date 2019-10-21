64b6ee5210a411ea48faab4b9dec320bcea84baa
thebioengineer <ellishughes@live.com>
2019-10-20 19:10:23 -0700

    add methods for testing docker_connection

--- tests/testthat/test-docker_conn.R
+++ tests/testthat/test-docker_conn.R
@@ -41,104 +41,46 @@ test_with_docker("A new docker_connection object can be generated using the
   expect_s3_class(test_conn, "docker_connection")
   expect_s3_class(test_conn_2, "docker_connection")
   expect_equal(test_conn_2, test_conn)
-
-
 })
 
-test_with_docker("docker_stop can stop a container through either a
-                 docker_connection or the name of the image", {
-
-  test_conn <- docker_run(image = "connection_test_image",
-                          name = "connection_test",
-                          docker_run_args = "-d")
-  test_conn_2 <- docker_run(image = "connection_test_image",
-                          name = "connection_test2",
-                          docker_run_args = "-d")
-
-  expect_message(docker_stop(test_conn), "Stopped Docker Container:")
-  expect_message(docker_stop("connection_test2"), "Stopped Docker Container:")
+test_with_docker("A `docker_connection` cannot be made for a container that does
+                 not exist",{
+  expect_error(docker_connection("Poorly_named_and_illegal_Dockyard_Container"),
+               "Must be a valid container name")
 })
 
-test_with_docker("docker_kill can kill a container through either a
-                 docker_connection or the name of the image", {
-
-  test_conn <- docker_run(image = "connection_test_image",
-                          name = "connection_test",
-                          docker_run_args = "-d")
-  test_conn_2 <- docker_run(image = "connection_test_image",
-                            name = "connection_test2",
-                            docker_run_args = "-d")
-
-  expect_message(docker_kill(test_conn), "Killed Docker Container:")
-  expect_message(docker_kill("connection_test2"), "Killed Docker Container:")
-})
-
-test_with_docker("docker_pause can pause a running container through either a
-                 docker_connection or the name of the image", {
-
-  test_conn <- docker_run(image = "connection_test_image",
-                          name = "connection_test",
-                          docker_run_args = "-d")
-  test_conn_2 <- docker_run(image = "connection_test_image",
-                            name = "connection_test2",
-                            docker_run_args = "-d")
+test_with_docker("Printing `docker_connection` lists all available info about
+                 the container; name, port, base images, status",{
   on.exit({
-    docker_stop(test_conn)
-    docker_stop(test_conn_2)
+    if("connection_test"%in%list_containers()$NAMES){
+      docker_stop("connection_test")
+    }
   })
 
-  ct_status <- grepl("Paused",get_container("connection_test")$STATUS)
-  ct2_status <-  grepl("Paused",get_container("connection_test2")$STATUS)
-
-
-  expect_message(docker_pause(test_conn), "Paused Docker Container:")
-  expect_message(docker_pause("connection_test2"), "Paused Docker Container:")
-
-  ct_status_post <- grepl("Paused",get_container("connection_test")$STATUS)
-  ct2_status_post <-  grepl("Paused",get_container("connection_test2")$STATUS)
-
-  expect_false(ct_status)
-  expect_false(ct2_status)
-  expect_true(ct_status_post)
-  expect_true(ct2_status_post)
-})
-
-test_with_docker("docker_pause can pause a running container through either a
-                 docker_connection or the name of the image", {
-
   test_conn <- docker_run(image = "connection_test_image",
                           name = "connection_test",
                           docker_run_args = "-d")
-  test_conn_2 <- docker_run(image = "connection_test_image",
-                            name = "connection_test2",
-                            docker_run_args = "-d")
 
-  on.exit({
-    docker_stop(test_conn)
-    docker_stop(test_conn_2)
-  })
+  conn_output <- capture.output({
+    print(test_conn)})
 
-  ct_status <- grepl("Paused",get_container("connection_test")$STATUS)
-  ct2_status <-  grepl("Paused",get_container("connection_test2")$STATUS)
+  docker_stop("connection_test")
 
-  docker_pause(test_conn)
-  docker_pause("connection_test2")
+  conn_output2 <- capture.output({
+    print(test_conn)})
 
-  ct_status_pause <- grepl("Paused",get_container("connection_test")$STATUS)
-  ct2_status_pause <-  grepl("Paused",get_container("connection_test2")$STATUS)
+  expect_true(grepl(
+    "[<] Docker Container[:] connection_test [|] Port[:] NA [|] Status[:] Up .+[|] image[:] connection_test_image [>]",
+    conn_output))
 
-  expect_message(docker_unpause(test_conn), "Unpaused Docker Container:")
-  expect_message(docker_unpause("connection_test2"), "Unpaused Docker Container:")
-
-  ct_status_unpause <- grepl("Paused",get_container("connection_test")$STATUS)
-  ct2_status_unpause <-  grepl("Paused",get_container("connection_test2")$STATUS)
-
-  expect_false(ct_status)
-  expect_false(ct2_status)
-  expect_true(ct_status_pause)
-  expect_true(ct2_status_pause)
-  expect_false(ct_status_unpause)
-  expect_false(ct_status_unpause)
-
+  expect_true(grepl(
+    "[<] Docker Container[:] connection_test [|] Port[:] N/A [|] Status[:] Does Not Exist [|] image[:] connection_test_image [>]",
+    conn_output2))
 
 })
+
+
+
+
+
+
